86cf34304a65579b1c79eec3806a84e6b01a5911
Kimminsu <alstn2468_@naver.com>
2020-04-17 10:30:32 +0900

    Add nomad coder python challenge day 5 using bs4

---
+++ Python_Challenge_Day_Five.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_int_input(length, prompt="#: "):
+    user_input = input(prompt).strip()
+
+    try:
+        user_input = int(user_input)
+
+        if user_input > (length - 1) or user_input < 0:
+            raise RuntimeError
+        return user_input
+    except ValueError:
+        return get_int_input(length, prompt="That wasn't a number.\n#: ")
+    except RuntimeError:
+        return get_int_input(length, prompt="Choose a number from the list.\n#: ")
+
+
+os.system("clear")
+url = "https://www.iban.com/currency-codes"
+
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+countries = soup.select("table > tbody > tr")
+data = [list(filter(lambda x: x != "\n", country.children))
+        for country in countries]
+filtered_data = list(
+    (
+        map(
+            lambda country: [country[0].text.capitalize(), country[-2].text],
+            filter(lambda country: country[1].text !=
+                   "No universal currency", data),
+        )
+    )
+)
+
+print("Hello! Pease choose select a country by number:")
+print("\n".join([f"# {idx:3} {val[0]}" for idx,
+                 val in enumerate(filtered_data)]))
+
+user_input = get_int_input(len(filtered_data))
+print(user_input)
+print(
+    f"""You choose {filtered_data[user_input][0]}
+The currency code is {filtered_data[user_input][1]}"""
+)
