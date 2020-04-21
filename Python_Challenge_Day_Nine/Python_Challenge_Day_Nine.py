4bec00e38db07ea8b2f753bbba4e5cddbb4f5a97
Kimminsu <alstn2468_@naver.com>
2020-04-21 14:07:48 +0900

    Add detail template html structure using data's children

--- Python_Challenge_Day_Nine/Python_Challenge_Day_Nine.py
+++ Python_Challenge_Day_Nine/Python_Challenge_Day_Nine.py
@@ -1,23 +1,16 @@
 import requests
 from flask import Flask, render_template, request
 
+
+app = Flask("DayNine")
 base_url = "http://hn.algolia.com/api/v1"
-
-# This URL gets the newest stories.
 new = f"{base_url}/search_by_date?tags=story"
-
-# This URL gets the most popular stories
 popular = f"{base_url}/search?tags=story"
+db = {}
 
 
-# This function makes the URL to get the detail of a storie by id.
-# Heres the documentation: https://hn.algolia.com/api
 def make_detail_url(id):
     return f"{base_url}/items/{id}"
-
-
-db = {}
-app = Flask("DayNine")
 
 
 @app.route("/")
@@ -52,7 +45,6 @@ def detail(object_id):
         detail_url = make_detail_url(object_id)
         response = requests.get(detail_url)
         data = response.json()
-        print(data["children"])
         return render_template("detail.html", object_id=object_id, data=data)
 
     except Exception:
